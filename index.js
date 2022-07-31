f2df545b4a10277c9a6cb057979878ce31aeddb8
Lea Eid <l.eid@acteos.com>
2022-07-31 13:09:50 +0300

    added CORS POLICY

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var app = express();
 
+// CORS POLICY
+app.use(function (req, res, next) {
+  const allowedOrigins = '';
+  const origin = req.headers.origin;
+  if (allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Headers', 'X-data, Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+
+  next();
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
